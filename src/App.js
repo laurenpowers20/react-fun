560fff18d327015e1a4250e04b713eb6692c37dd
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-01-26 22:02:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo3 from "./images/react.png";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App-header">
+ 
+        <h1 className="welcome">Welcome to React Fun</h1>
+        <img src={logo3} className="App-logo" alt="logo" />
+      </div>
+
   );
 }
 
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <>
+      <div className="nav-component">
+        <h1>Nav</h1>
+        {/* <div className="nav-link-container">
+          <NavLink className="navlink" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navlink" to="/searchbar">
+            Search Bar
+          </NavLink>
+        </div> */}
+      </div>
+    </>
+  );
+}
+
+export default Nav;
